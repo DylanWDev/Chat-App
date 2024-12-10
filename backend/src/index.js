ef816fda1daabb43c5d9cb635f8bd1b9571f1e31
DylanWDev <xpoweroutage@gmail.com>
2024-12-09 23:40:49 -0500

    connected MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
+  connectDB()
 });
