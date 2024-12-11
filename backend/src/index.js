77733cfe7dfd228d49128aec34086e8a1cd3ab36
DylanWDev <xpoweroutage@gmail.com>
2024-12-10 23:31:16 -0500

    finished setting up signUp functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,11 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
-  connectDB()
+  connectDB();
 });
