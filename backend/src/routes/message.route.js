b78ee1cf3b9b97dd34aace39bd5f01faa61b7ce2
DylanWDev <xpoweroutage@gmail.com>
2024-12-16 22:44:22 -0500

    added messages routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,5 +5,8 @@ import { getUsersForSidebar } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
